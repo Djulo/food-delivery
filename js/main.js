1c28b55e8539a003fdbf5039b7ec652054ae6295
Nikola Kovacevic <nikovacevicc@gmail.com>
2019-06-18 01:40:16 +0200

    Restorani se ocitavaju dinamicki, ubaceni filteri i sortiranje

---
+++ js/main.js
@@ -0,0 +1,155 @@
+
+var data = {
+    "restorani": [
+        {
+            "id": 1,
+            "naziv": "Restoran 1",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Srpska hrana"],
+            "adresa": "Toplicka 22, Beograd",
+            "grad": "Beograd",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 1000,
+            "cena_dostave": "Besplatna dostava",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2]
+        },
+        {
+            "id": 2,
+            "naziv": "Restoran 2",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Srpska hrana", "Italijanska hrana"],
+            "adresa": "Voislava Ilica 15, Beograd",
+            "grad": "Beograd",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 600,
+            "cena_dostave": "250 RSD",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 4, 5, 4, 2, 5, 5, 1, 2]
+        },
+        {
+            "id": 3,
+            "naziv": "Restoran 3",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Italijanska hrana"],
+            "adresa": "Milesevska 67, Beograd",
+            "grad": "Beograd",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 500,
+            "cena_dostave": "Besplatna dostava",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2, 5, 5, 5, 5, 4]
+        },
+        {
+            "id": 4,
+            "naziv": "Restoran 4",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Srpska hrana", "Italijanska hrana"],
+            "adresa": "Sazonova 116, Beograd",
+            "grad": "Beograd",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 600,
+            "cena_dostave": "Besplatna dostava",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2, 1, 3]
+        }
+    ]
+};
+
+if(!localStorage.getItem('globalData')){
+    localStorage.setItem('globalData', JSON.stringify(data));
+}
+
+function dohvatiOcenu(restoran){
+    var sum = 0;
+    for (var i=0; i<restoran.ocene.length; i++) sum += restoran.ocene[i];
+    return Math.round((sum / restoran.ocene.length) * 100) / 100;;
+}
