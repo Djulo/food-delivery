644c54b74e4ae6532ac7ac0a288c209d861438c4
Nikola Kovacevic <nikovacevicc@gmail.com>
2019-06-18 22:20:37 +0200

    Added english langugage

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ var data = {
     "restorani": [
         {
             "id": 1,
-            "naziv": "Restoran 1",
+            "naziv": "Kovac",
             "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "kuhinje": ["Srpska hrana"],
+			"opstine": ["Novi Beograd"],
             "adresa": "Toplicka 22, Beograd",
             "grad": "Beograd",
-            "slika": ["restoran1.jfif"],
+            "slika": ["restoran1.jfif", "kovac1.jpg", "kovac2.jpg", "kovac3.jpg"],
             "min_dostava": 1000,
-            "cena_dostave": "Besplatna dostava",
+			"cena_dostave": "Besplatna dostava",
             "jela": [
                 {
                     "naziv": "Hamburger",
@@ -41,6 +42,7 @@ var data = {
             "naziv": "Restoran 2",
             "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "kuhinje": ["Srpska hrana", "Italijanska hrana"],
+			"opstine": ["Zvezdara"],
             "adresa": "Voislava Ilica 15, Beograd",
             "grad": "Beograd",
             "slika": ["restoran1.jfif"],
@@ -76,6 +78,7 @@ var data = {
             "naziv": "Restoran 3",
             "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "kuhinje": ["Italijanska hrana"],
+			"opstine": ["Vracar"],
             "adresa": "Milesevska 67, Beograd",
             "grad": "Beograd",
             "slika": ["restoran1.jfif"],
@@ -111,6 +114,7 @@ var data = {
             "naziv": "Restoran 4",
             "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "kuhinje": ["Srpska hrana", "Italijanska hrana"],
+			"opstine": ["Vozdovac"],
             "adresa": "Sazonova 116, Beograd",
             "grad": "Beograd",
             "slika": ["restoran1.jfif"],
@@ -146,6 +150,160 @@ var data = {
 
 if(!localStorage.getItem('globalData')){
     localStorage.setItem('globalData', JSON.stringify(data));
+}
+
+
+var data_en = {
+    "restorani": [
+        {
+            "id": 1,
+            "naziv": "Kovac",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Serbian food"],
+			"opstine": ["Novi Beograd"],
+            "adresa": "Toplicka 22, Belgrade",
+            "grad": "Belgrade",
+            "slika": ["restoran1.jfif", "kovac1.jpg", "kovac2.jpg", "kovac3.jpg"],
+            "min_dostava": 1000,
+			"cena_dostave": "Free delivery",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2]
+        },
+        {
+            "id": 2,
+            "naziv": "Restaurant 2",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Serbian food", "Italian food"],
+			"opstine": ["Zvezdara"],
+            "adresa": "Voislava Ilica 15, Belgrade",
+            "grad": "Belgrade",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 600,
+            "cena_dostave": "250 RSD",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 4, 5, 4, 2, 5, 5, 1, 2]
+        },
+        {
+            "id": 3,
+            "naziv": "Restaurant 3",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Italian food"],
+			"opstine": ["Vracar"],
+            "adresa": "Milesevska 67, Belgrade",
+            "grad": "Beograd",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 500,
+            "cena_dostave": "Free delivery",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2, 5, 5, 5, 5, 4]
+        },
+        {
+            "id": 4,
+            "naziv": "Restaurant 4",
+            "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "kuhinje": ["Serbian food", "Italian food"],
+			"opstine": ["Vozdovac"],
+            "adresa": "Sazonova 116, Beograd",
+            "grad": "Belgrade",
+            "slika": ["restoran1.jfif"],
+            "min_dostava": 600,
+            "cena_dostave": "Free delivery",
+            "jela": [
+                {
+                    "naziv": "Hamburger",
+                    "kategorija": "Hamburger",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 320,
+                    "slika": "hamburger.jfif"
+                },
+                {
+                    "naziv": "Margarita",
+                    "kategorija": "Pizza",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 400,
+                    "slika": "margarita.jpeg"
+                },
+                {
+                    "naziv": "Carbonara",
+                    "kategorija": "Pasta",
+                    "sastojci": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    "cena": 360,
+                    "slika": "carbonara.jfif"
+                }
+            ],
+            "ocene": [5, 5, 3, 5, 4, 2, 1, 3]
+        }
+    ]
+};
+
+if(!localStorage.getItem('globalData-en')){
+    localStorage.setItem('globalData-en', JSON.stringify(data_en));
 }
 
 function dohvatiOcenu(restoran){
